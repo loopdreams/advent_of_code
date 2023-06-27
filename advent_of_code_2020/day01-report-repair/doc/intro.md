38dfa79db11e802e104f77febcdaa1a107709129
Eoin Carney <eoincarney0@gmail.com>
2023-06-27 19:46:49 +0100

    added solution 2020 day 1

---
+++ advent_of_code_2020/day01-report-repair/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to day01-report-repair
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ advent_of_code_2020/day01-report-repair/src/day01_report_repair/core.clj
@@ -0,0 +1,38 @@
+(ns day01-report-repair.core
+  (:require [clojure.string :as str]))
+
+
+(def sample (slurp "sample.txt"))
+(def input (slurp "input.txt"))
+
+(defn parse-input [input]
+  (->> input str/split-lines (map #(Integer/parseInt %))))
+
+(defn part1 [nums]
+  (reduce * (for [n nums
+                  :let [sums (map #(+ n %) (remove #{n} nums))]
+                  :when ((set sums) 2020)]
+              n)))
+
+(comment
+  (part1 (parse-input input)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PART 2
+
+
+(defn part2 [nums]
+  (->> nums
+       (map
+        (fn [val]
+          (let [pair-vals (map #(+ val %) (remove #{val} nums))
+                three-vals (for [x pair-vals
+                                 y (remove #{val} nums)]
+                             (+ x y))]
+            (when ((set three-vals) 2020)
+              val))))
+       (remove nil?)
+       (reduce *)))
+
+(comment
+  (part2 (parse-input input)))
