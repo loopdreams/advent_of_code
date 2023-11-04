a40950eb944a7bdbb3aab9e4a8779af349727edc
Eoin Carney <eoincarney0@gmail.com>
2023-11-04 18:44:42 +0000

    added solution 2018 day 1

---
+++ advent_of_code_2018/README.md
@@ -0,0 +1,3 @@
+# Advent Of Code 2018
+
+- [Day 01 - Chronal Calibration](day01-chronal-calibration/src/day01_chronal_calibration/core.clj)
---
+++ advent_of_code_2018/day01-chronal-calibration/src/day01_chronal_calibration/day01_chronal_calibration.clj
@@ -0,0 +1,31 @@
+(ns day01-chronal-calibration
+  (:require [clojure.string :as str]))
+
+(def input (slurp "input.txt"))
+
+(defn parse-input [input]
+  (map parse-long (str/split-lines input)))
+
+(defn calculate-frequency [input]
+  (let [steps (parse-input input)]
+    (reduce (fn [acc e]
+              (+ acc e))
+            0
+            steps)))
+            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PART 2
+
+(defn find-repeating-frequency [nums]
+  (loop [[n & ns] nums
+         fq #{0}
+         active-fq 0]
+    (if n
+      (let [n-fq (+ active-fq n)
+            new-list (conj fq active-fq)]
+        (if (new-list n-fq) n-fq
+            (recur ns new-list n-fq)))
+      (recur nums fq active-fq))))
+
+(comment
+  (find-repeating-frequency (parse-input input)))
